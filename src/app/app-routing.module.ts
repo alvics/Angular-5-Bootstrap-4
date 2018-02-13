d3a1ab2255faed7e19d8a21d504d5ed70c1330af
alvics <alvics71@hotmail.com>
2018-02-13 14:24:26 +1000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent} from './about/about.component';
+import { BlogComponent} from './blog/blog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: 'home', component: HomeComponent },
+{ path: 'about', component: AboutComponent },
+{ path: 'contact', component: ContactComponent },
+{ path: 'gallery', component: GalleryComponent },
+{ path: 'blog', component: BlogComponent },
+{ path: '',   redirectTo: '/home', pathMatch: 'full'},
+{ path: '**', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    GalleryComponent,
+    BlogComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
